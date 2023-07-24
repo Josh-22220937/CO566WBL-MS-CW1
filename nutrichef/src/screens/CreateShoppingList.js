a2b40a7bd87dd158f609d2a3c31e164d71cdfca1
Josh Harris <132237476+Josh-22220937@users.noreply.github.com>
2023-07-24 12:57:59 +0100

    Update CreateShoppingList.js

--- nutrichef/src/screens/CreateShoppingList.js
+++ nutrichef/src/screens/CreateShoppingList.js
@@ -1,10 +1,13 @@
+// Import React library
 import React from 'react';
 
+// handle the fact that no functionality is on this scren
 const CreateShoppingList = () => {
   const handleScreenClick = () => {
     alert('This feature is not implemented at the moment.');
   };
 
+  // Main return, defines structure of screen
   return (
     <div className="screen" onClick={handleScreenClick}>
     <h1>NutriChef</h1>
