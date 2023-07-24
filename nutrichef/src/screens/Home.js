5a131e08dbb2f46b0daff44846afa61936c14de8
Josh Harris <132237476+Josh-22220937@users.noreply.github.com>
2023-07-24 12:56:46 +0100

    Update Home.js

--- nutrichef/src/screens/Home.js
+++ nutrichef/src/screens/Home.js
@@ -1,14 +1,17 @@
+// Import React library, react-router-dom Link, necessary component and styles
 import React from 'react';
 import { Link } from 'react-router-dom';
 import RecipeTable from '../components/RecipeTable';
 import '../styles/Home.css'
 
+// Define the Home screen
 const Home = ({ recipes, ingredients, removeRecipe }) => {
   return (
     
     <div className="screen">
       <h1>NutriChef</h1>
       <h2>3 Most recent Recipes</h2>
+      // Making use of the recipetable component 
       <RecipeTable recipes={recipes} ingredients={ingredients} removeRecipe={removeRecipe} numberOfRecipes={3}/>
       
       <div className="navigation-buttons">
